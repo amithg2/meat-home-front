728574cf2f4d10581144ad68c3b83242c2bb3274
amithg2 <amitadi91@gmail.com>
2021-12-07 12:30:08 +0200

    added small statistics

--- src/components-admin/Statistics.js
+++ src/components-admin/Statistics.js
@@ -1,54 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withStyles } from "@material-ui/styles";
-
+import axios from "axios";
 const styles = {
   statistics: {
-    backgroundColor: "lightcyan",
     width: "100%",
     height: "40vh",
-    display : 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center'
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginTop: "1rem",
+    textAlign: "center",
   },
   card: {
     width: "30%",
     height: "10vh",
     border: "1px solid black",
-    margin: '0.3rem',
-    background : 'lightgrey',
-    textAlign: 'center'
+    margin: "0.3rem",
+    background: "lightgrey",
+    boxShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px",
   },
 };
 
 
-//   //
-  //   const { numOfPeopleTotal, numOfResTotal } = dataStats;
-  //   const average = Math.round(numOfPeopleTotal / numOfResTotal);
-  //   console.log(numOfPeopleTotal, numOfResTotal);
-  //   console.log(average);
+function Statistics({ classes, reservations }) {
+  const [enters, setEnters] = useState("");
 
-//   await new DataModel({
-  //     resIds: [1],
-  //     enters: 0,
-  //     madeRes: 0,
-  //     numOfPeopleAvg: 1,
-  //     numOfPeopleTotal: 1,
-  //     numOfResTotal: 1,
-  //   }).save();
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get("/admin");
 
-  
+      setEnters(data.statistics[0].enters);
+    };
+    getData();
+  }, []);
 
-
-function Statistics(props) {
-  console.log(props);
-
-  const { classes, reservations } = props;
-
-  
+  let totalCounter = 0;
+  const showNumOfPplTotal = () => {
+    if (reservations.length) {
+      reservations.forEach((e) => {
+        if (e.numOfPeopleRes) {
+          return (totalCounter += parseInt(e.numOfPeopleRes));
+        }
+      });
+      return totalCounter;
+    }
+  };
+  const showApproved = () => {
+    let approvedCounter = 0;
+    if (reservations.length) {
+      reservations.forEach((e) => {
+        if (e.isApproved) approvedCounter++;
+      });
+    }
+    return approvedCounter;
+  };
+  const showAvgNumOfPpl = () => {
+    const avg = Math.round(totalCounter / reservations.length);
+    return avg;
+  };
+  const showResEntersPrecent = () =>
+    Math.round((reservations.length / enters) * 100);
   return (
-    <div className={classes.statistics}>
-     
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>סטטיסטיקה :</h1>
+      <div className={classes.statistics}>
+        <div className={classes.card}>
+          סך הכל אנשים בכל הההזמנות : {showNumOfPplTotal()}
+        </div>
+        <div className={classes.card}>
+          מספר הזמנות שאושרו : {showApproved()}
+        </div>
+        <div className={classes.card}>מספר הזמנות : {reservations.length}</div>
+
+        <div className={classes.card}>
+          ממוצע אנשים לאירוע : {showAvgNumOfPpl()}
+        </div>
+        <div className={classes.card}>כניסות לאתר :{enters}</div>
+        <div className={classes.card}>
+          אחוז מזמינים מתוך נכנסים לאתר :{showResEntersPrecent()}
+        </div>
+      </div>
+    </>
   );
 }
 
